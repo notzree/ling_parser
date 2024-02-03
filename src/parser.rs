432dae27fc7c2f338187c0dcf6599d6cbfb28157
notzree <notzree@gmail.com>
2024-02-03 16:58:25 -0500

    set up parser

---
+++ src/parser.rs
@@ -0,0 +1,10 @@
+use std::collections::HashMap;
+
+use crate::constants::*;
+
+pub fn parse(
+    tokens: Vec<TokenType>,
+) -> Result<HashMap<String, String>, Box<dyn std::error::Error>> {
+    //todo: implement parser logic
+    Ok({})
+}
